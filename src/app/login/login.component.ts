d37f3808fb72c99e07fd53f8d2d0c7e0a56eaac8
Holti Kryemadhi <holti.k@outlook.com>
2024-04-27 18:19:26 +0200

    Login form, started with register

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
     selector: 'app-login',
     standalone: true,
-    imports: [],
+    imports: [CommonModule, ReactiveFormsModule, FloatLabelModule, ButtonModule ],
     templateUrl: './login.component.html',
     styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+    loginForm = new FormGroup({
+        email: new FormControl('', [
+            // input validation
+            Validators.required,
+            Validators.email
+        ]),
+        password: new FormControl('', [
+            Validators.required,
+        ])
+    });
+    onSubmit(){ // what happens when button is pressed
+        if (this.loginForm.invalid){
+            console.log("invalid");
+        }
+        console.log("submitted");
+    }
+    get email(){
+        return this.loginForm.get("email");
+    }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,55 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { ButtonModule } from 'primeng/button';
+import { RadioButtonModule } from 'primeng/radiobutton';
+
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, ReactiveFormsModule, FloatLabelModule, ButtonModule, RadioButtonModule ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
 
+export class RegisterComponent {
+  registerForm = new FormGroup({
+      firstName: new FormControl('', [
+          // input validation
+          Validators.required,
+      ]),
+      lastName: new FormControl('', [
+          Validators.required,
+      ]),
+
+      title: new FormControl('', [
+       
+      ]),
+      email: new FormControl('', [
+        // input validation
+        Validators.required,
+        Validators.email
+    ]),
+    password: new FormControl('', [
+        Validators.required,
+    ]),
+
+  });
+  onSubmit(){ // what happens when button is pressed
+      if (this.registerForm.invalid){
+          console.log("invalid");
+      }
+      console.log("submitted");
+  }
+  selectOtherOption(){
+    this.registerForm.get('title')?.setValue('Other');
+  }
+  get email(){
+      return this.registerForm.get("email");
+  }
+  get title(){
+    return this.registerForm.get("title");
+  }
 }
