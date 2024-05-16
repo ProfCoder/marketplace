3362a1e63c973b35a40778d3f82293e02b58368c
almaz.baghirova <almaz.baghirova@uni-konstanz.de>
2024-05-16 18:23:52 +0200

    Implemented ProductDetailComponent to subscribe to route parameters and fetch product data based on the product id

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { ProductService } from '../services/product.service';
 import { Product } from '../services/product';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule], 
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']  
 })
@@ -20,16 +20,19 @@ export class ProductDetailComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    const productId = this.route.snapshot.params['id'];
-    console.log(`Product ID: ${productId}`);
-    this.productService.getProductMetadata(productId).subscribe((product: Product) => {
-      console.log(`Product Data: ${JSON.stringify(product)}`); 
-      this.product = product;
-      if (!this.product.description) {
-        this.product.description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
-      }
-    }, (error) => {
-      console.error('Error loading product:', error);
+    this.route.params.subscribe((params: Params) => {
+      const productId = params['id'];
+      console.log(`Product ID: ${productId}`); 
+      this.productService.getProductMetadata(productId).subscribe((product: Product) => {
+        console.log(`Product Data: ${JSON.stringify(product)}`);
+        this.product = product;
+        // Adding default description (Lorem Ipsum text)
+        if (!this.product.description) {
+          this.product.description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+        }
+      }, (error) => {
+        console.error('Error loading product:', error); 
+      });
     });
   }
 }
