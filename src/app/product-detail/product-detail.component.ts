cdec4fafdd6de86bb22fb64c0e858dc277e5ba1b
almaz.baghirova <almaz.baghirova@uni-konstanz.de>
2024-05-19 23:52:42 +0200

    Implemented color_name value in the url

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -23,45 +23,13 @@ export class ProductDetailComponent implements OnInit {
   product!: ProductWithRating;
   selectedSize: string | null = null;
   selectedColor: { color_id: string, color_name: string; color_hex: string } | null = null;
+  productImageSrc: string | null = null;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     public productService: ProductService
   ) {}
-
-  // ngOnInit(): void {
-  //   this.route.params.subscribe((params: Params) => {
-  //     const productId = params['id'];
-  //     console.log(`Product ID: ${productId}`);
-  //     this.productService.getProductMetadata(productId).subscribe((product: Product) => {
-  //       console.log(`Product Data: ${JSON.stringify(product)}`);
-  //       const productWithRating: ProductWithRating = {
-  //         ...product,
-  //         rating: parseFloat((Math.random() * (5 - 1) + 1).toFixed(1)),
-  //         ratingQuantity: this.getRandomInt(1500, 7500)
-  //       };
-  //       this.product = productWithRating;
-  //       if (!this.product.description) {
-  //         this.product.description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
-  //       }
-  //       if (this.product.sizes && this.product.sizes.length > 0) {
-  //         this.selectedSize = this.product.sizes[0];
-  //       }
-  //       this.route.queryParams.subscribe((queryParams) => {
-  //         const colorId = queryParams['color'];
-  //         if (colorId) {
-  //           this.selectColorById(colorId);
-  //         } else {
-  //           this.selectedColor = null;
-  //         }
-  //       });
-  //     }, (error) => {
-  //       console.error('Error loading product:', error);
-  //     });
-  //   });
-  // }
-
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
@@ -79,12 +47,11 @@ export class ProductDetailComponent implements OnInit {
           this.product.description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
         }
 
-        // Check if size is specified in query parameters and set it as selected
         this.route.queryParams.subscribe((queryParams) => {
           console.log('Query Params:', queryParams);
-          const colorId = queryParams['color'];
-          if (colorId) {
-            this.selectColorById(colorId);
+          const colorName = queryParams['color'];
+          if (colorName) {
+            this.selectColorByName(colorName);
           } else {
             this.selectedColor = null;
           }
@@ -94,17 +61,17 @@ export class ProductDetailComponent implements OnInit {
           if (sizeParam && this.product.sizes.includes(sizeParam)) {
             this.selectedSize = sizeParam;
           } else if (this.product.sizes && this.product.sizes.length > 0) {
-            this.selectedSize = this.product.sizes[0]; // Default to first size if no size parameter is provided or it's invalid
+            this.selectedSize = this.product.sizes[0]; 
           }
           console.log('Selected Size:', this.selectedSize);
+          this.updateProductImage();
         });
       }, (error) => {
         console.error('Error loading product:', error);
       });
     });
   }
-  
-  
+
   getRandomInt(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
@@ -115,15 +82,16 @@ export class ProductDetailComponent implements OnInit {
 
   selectSize(size: string): void {
     this.selectedSize = size;
+    this.updateUrl();
   }
 
   selectColor(color: { color_id: string, color_name: string, color_hex: string }): void {
     this.selectedColor = color;
+    this.updateUrl();
   }
 
-
-selectColorById(colorId: string): void {
-    const color = this.product.colors.find(c => c.color_id === colorId);
+  selectColorByName(colorName: string): void {
+    const color = this.product.colors.find(c => c.color_name === colorName);
     if (color) {
       this.selectedColor = color;
     } else {
@@ -131,38 +99,48 @@ selectColorById(colorId: string): void {
     }
   }
 
-
   onColorSelect(color: { color_id: string, color_name: string, color_hex: string }): void {
     this.router.navigate([], {
-        relativeTo: this.route,
-        queryParams: { color: color.color_id },
-        queryParamsHandling: 'merge',
+      relativeTo: this.route,
+      queryParams: { color: color.color_name, size: this.selectedSize },
+      queryParamsHandling: 'merge',
     });
-}
+  }
 
   navigateHome(): void {
     this.router.navigate(['/']);
   }
 
-
   onSizeChange(event: any): void {
     const selectedSize = event.target.value;
     this.selectedSize = selectedSize;
-    
-    const queryParams: any = { size: selectedSize };
+    this.updateUrl();
+  }
 
+  updateUrl(): void {
+    const queryParams: any = {};
     if (this.selectedColor) {
-      queryParams.color = this.selectedColor.color_id;
+      queryParams.color = this.selectedColor.color_name;
     }
-  
+    if (this.selectedSize) {
+      queryParams.size = this.selectedSize;
+    }
+
     this.router.navigate([], {
       relativeTo: this.route,
       queryParams,
       queryParamsHandling: 'merge',
     });
   }
-  
-  
-  
-  
+
+  updateProductImage(): void {
+    if (this.selectedColor) {
+      const color = this.product.colors.find(c => c.color_name === this.selectedColor?.color_name);
+      if (color) {
+        this.productImageSrc = this.productService.getProductImage(color.color_id);
+      }
+    } else {
+      this.productImageSrc = this.productService.getProductImage(this.product.id);
+    }
+  }
 }
