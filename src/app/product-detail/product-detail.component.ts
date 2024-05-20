0412d1c7a7ca69590b16fdd89085290d8691ec93
almaz.baghirova <almaz.baghirova@uni-konstanz.de>
2024-05-20 23:20:13 +0200

    Implemented Scroll to top button

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, HostListener } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { ProductService } from '../services/product.service';
 import { Product } from '../services/product';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ImageModule } from 'primeng/image';
+import { ButtonModule } from 'primeng/button';
 
 interface ProductWithRating extends Product {
   rating: number;
@@ -16,48 +17,56 @@ interface Feedback {
   rating: number;
   stars: number[];
   userName: string;
-  userId?: string; 
+  userId?: string;
 }
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [CommonModule, RouterModule, ImageModule],
+  imports: [CommonModule, RouterModule, ImageModule, ButtonModule],
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class ProductDetailComponent implements OnInit {
   product!: ProductWithRating;
   selectedSize: string | null = null;
-  selectedColor: { color_id: string, color_name: string; color_hex: string } | null = null;
+  selectedColor: { color_id: string; color_name: string; color_hex: string } | null = null;
   productImageSrc: string | null = null;
-  selectedQuantity: number = 1; 
-  recommendedProducts: Product[] = []; 
-  
+  selectedQuantity: number = 1;
+  recommendedProducts: Product[] = [];
   feedbacks: Feedback[] = [];
+  showScrollButton: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    public productService: ProductService,
-    ) {
-      const lastTenPresidents = [
-        "Joe Biden", "Donald Trump", "Barack Obama", "George W. Bush", "Bill Clinton",
-        "George H. W. Bush", "Ronald Reagan", "Jimmy Carter", "Gerald Ford", "Richard Nixon"
-      ];
-      // Generate 10 feedbacks
-      for (let i = 1; i < 11; i++) {
-        const rating = Math.ceil(Math.random() * 10);
-        const comment = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
-        const starRating = +(rating / 2).toFixed(1);
-        const stars = Array.from({ length: 5 }, (_, index) => index < starRating ? 1 : 0);
-        const userName = lastTenPresidents[i];
-        const userId = `${i}`; // Unique identifier
-        this.feedbacks.push({ comment, rating, stars, userName, userId });
-      }
+    public productService: ProductService
+  ) {
+    const lastTenPresidents = [
+      'Joe Biden',
+      'Donald Trump',
+      'Barack Obama',
+      'George W. Bush',
+      'Bill Clinton',
+      'George H. W. Bush',
+      'Ronald Reagan',
+      'Jimmy Carter',
+      'Gerald Ford',
+      'Richard Nixon',
+    ];
+    // Generate 10 feedbacks
+    for (let i = 1; i < 11; i++) {
+      const rating = Math.ceil(Math.random() * 10);
+      const comment =
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
+      const starRating = +(rating / 2).toFixed(1);
+      const stars = Array.from({ length: 5 }, (_, index) => (index < starRating ? 1 : 0));
+      const userName = lastTenPresidents[i];
+      const userId = `${i}`; // Unique identifier
+      this.feedbacks.push({ comment, rating, stars, userName, userId });
     }
-
+  }
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
@@ -69,30 +78,31 @@ export class ProductDetailComponent implements OnInit {
           const productWithRating: ProductWithRating = {
             ...product,
             rating: parseFloat((Math.random() * (5 - 1) + 1).toFixed(1)),
-            ratingQuantity: this.getRandomInt(1500, 7500)
+            ratingQuantity: this.getRandomInt(1500, 7500),
           };
           this.product = productWithRating;
           if (!this.product.description) {
-            this.product.description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
+            this.product.description =
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
           }
-    
+
           this.route.queryParams.subscribe((queryParams) => {
             console.log('Query Params:', queryParams);
             const colorName = queryParams['color'];
             if (colorName) {
               this.selectColorByName(colorName);
             } else if (this.product.colors.length > 0) {
-              this.selectedColor = this.product.colors[0]; 
+              this.selectedColor = this.product.colors[0];
             } else {
               this.selectedColor = null;
             }
-    
+
             const sizeParam = queryParams['size'];
             console.log('Size Param:', sizeParam);
             if (sizeParam && this.product.sizes.includes(sizeParam)) {
               this.selectedSize = sizeParam;
             } else if (this.product.sizes && this.product.sizes.length > 0) {
-              this.selectedSize = this.product.sizes[0]; 
+              this.selectedSize = this.product.sizes[0];
             }
             console.log('Selected Size:', this.selectedSize);
             this.updateProductImage();
@@ -107,13 +117,22 @@ export class ProductDetailComponent implements OnInit {
     });
   }
   
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.showScrollButton = window.scrollY > 0;
+  }
+
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
   getUserImage(userId: string) {
     if (userId) {
       return `./assets/products/images/users/${userId}.png`;
     }
     return './assets/products/images/users/1.png';
   }
-  
+
   getRandomInt(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
@@ -126,14 +145,14 @@ export class ProductDetailComponent implements OnInit {
     this.selectedSize = size;
     this.updateUrl();
   }
-  
-  selectColor(color: { color_id: string, color_name: string, color_hex: string }): void {
+
+  selectColor(color: { color_id: string; color_name: string; color_hex: string }): void {
     this.selectedColor = color;
     this.updateUrl();
   }
 
   selectColorByName(colorName: string): void {
-    const color = this.product.colors.find(c => c.color_name === colorName);
+    const color = this.product.colors.find((c) => c.color_name === colorName);
     if (color) {
       this.selectedColor = color;
     } else {
@@ -141,7 +160,7 @@ export class ProductDetailComponent implements OnInit {
     }
   }
 
-  onColorSelect(color: { color_id: string, color_name: string, color_hex: string }): void {
+  onColorSelect(color: { color_id: string; color_name: string; color_hex: string }): void {
     this.router.navigate([], {
       relativeTo: this.route,
       queryParams: { color: color.color_name, size: this.selectedSize },
@@ -181,19 +200,15 @@ export class ProductDetailComponent implements OnInit {
     });
   }
 
-
   updateProductImage(): void {
     if (this.selectedColor) {
-      const color = this.product.colors.find(c => c.color_name === this.selectedColor?.color_name);
-      if (color) {
-        this.productImageSrc = this.productService.getProductImage(color.color_id);
+      const color = this.product.colors.find((c) => c.color_name === this.selectedColor?.color_name);
+      if (color) {        this.productImageSrc = this.productService.getProductImage(color.color_id);
       }
     } else {
       this.productImageSrc = this.productService.getProductImage(this.product.id);
     }
   }
-
-    
 
   decreaseQuantity(): void {
     if (this.selectedQuantity > 0) {
@@ -211,32 +226,34 @@ export class ProductDetailComponent implements OnInit {
     if (this.product && this.product.price) {
       return this.product.price * this.selectedQuantity;
     }
-    return 0  }
-  
-    addToBasket(): void {
-      // Logic to add the product will be done for the next assignments 
-    }
-  
-    buyNow(): void {
-      // Logic to buy product will be done for the next assignments 
-    }
-  
-    // Method to load recommended products
-    loadRecommendedProducts(): void {
-      const { type, category, id } = this.product;
-  
-      this.productService.getRecommendedProducts(type, category, id).subscribe(
-        (recommendedProducts: Product[]) => {
-          this.recommendedProducts = recommendedProducts;
-        },
-        (error) => {
-          console.error('Error loading recommended products:', error);
-        }
-      );
-    }
-    onRecommendedProductClick(recommendedProduct: Product): void {
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-      this.router.navigate(['/products', recommendedProduct.id]);
-    }
+    return 0;
   }
-  
+
+  addToBasket(): void {
+    // Logic to add the product will be done for the next assignments
+  }
+
+  buyNow(): void {
+    // Logic to buy product will be done for the next assignments
+  }
+
+  // Method to load recommended products
+  loadRecommendedProducts(): void {
+    const { type, category, id } = this.product;
+
+    this.productService.getRecommendedProducts(type, category, id).subscribe(
+      (recommendedProducts: Product[]) => {
+        this.recommendedProducts = recommendedProducts;
+      },
+      (error) => {
+        console.error('Error loading recommended products:', error);
+      }
+    );
+  }
+
+  onRecommendedProductClick(recommendedProduct: Product): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    this.router.navigate(['/products', recommendedProduct.id]);
+  }
+}
+
