1342bd755adcba6614baee380b18b87a2dba1a95
almaz.baghirova <almaz.baghirova@uni-konstanz.de>
2024-06-11 00:01:48 +0200

    1.Implemented 0 (Delete) option for cart quantity. 2. Implemented 10+ input field for quantity filed. 3. Solved issue with quantity field for products/carts  with same cart/product with different sizes

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -10,22 +10,21 @@ interface ExtendedProduct extends Product {
   color: string;
   size: string;
   quantity: number;
+  customQuantity?: boolean;
+  colorName: string;
 }
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [ CommonModule, RouterModule ],
+  imports: [CommonModule, RouterModule],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.css'
+  styleUrls: ['./cart.component.css']
 })
-
-
 export class CartComponent implements OnInit {
-[x: string]: any;
   cartItems: CartItem[] = [];
-  products: any[] = [];
-  quantityOptions: number[] = Array.from({ length: 10 }, (_, i) => i + 1);
+  products: ExtendedProduct[] = [];
+  quantityOptions: number[] = Array.from({ length: 11 }, (_, i) => i); 
 
   constructor(private cartService: CartService, private productService: ProductService) {}
 
@@ -34,7 +33,7 @@ export class CartComponent implements OnInit {
     this.loadProductDetails();
   }
 
- loadProductDetails() {
+  loadProductDetails() {
     this.productService.getAllProductMetadata().subscribe(products => {
       this.cartItems.forEach(item => {
         const product = products.find(p => p.id === item.id);
@@ -42,56 +41,54 @@ export class CartComponent implements OnInit {
           this.products.push({
             ...product,
             color: item.color,
-            colorName: item.colorName,
+            colorName: item['colorName'],
             size: item.size,
-            quantity: item.quantity
-          } as ExtendedProduct);
+            quantity: item.quantity,
+            customQuantity: item.quantity > 10
+          });
         }
       });
     });
   }
 
-  // changeQuantity(product: any, change: number, event: MouseEvent) {
-  //   event.stopPropagation();
-
-  //   const index = this.products.findIndex(p => p.id === product.id && p.color === product.color);
-  //   if (index > -1) {
-  //     this.products[index].quantity += change;
-  //     if (this.products[index].quantity < 1) {
-  //       this.removeProduct(this.products[index]);
-  //     } else {
-  //       this.cartService.setCartItem(this.products[index]);
-  //     }
-  //   }
-  // }
-
   changeQuantity(product: ExtendedProduct, event: any) {
     const newQuantity = parseInt(event.target.value);
-    const index = this.products.findIndex(p => p.id === product.id && p.color === product.color);
+    const index = this.products.findIndex(p => p.id === product.id && p.color === product.color && p.size === product.size);
     if (index > -1) {
-      this.products[index].quantity = newQuantity;
-      if (newQuantity < 1) {
+      if (newQuantity === 0) {
         this.removeProduct(this.products[index]);
+      } else if (newQuantity === 10) {
+        this.products[index].customQuantity = true;
       } else {
-        this.cartService.setCartItem(this.products[index]);
+        this.products[index].quantity = newQuantity;
+        this.cartService.setCartItem(this.mapToCartItem(this.products[index]));
+        this.products[index].customQuantity = false;
       }
     }
   }
-  
+
+  handleCustomQuantity(product: ExtendedProduct, event: any) {
+    const newQuantity = parseInt(event.target.value);
+    const index = this.products.findIndex(p => p.id === product.id && p.color === product.color && p.size === product.size);
+    if (index > -1) {
+      if (newQuantity > 0) {
+        this.products[index].quantity = newQuantity;
+        this.products[index].customQuantity = newQuantity > 10;
+        this.cartService.setCartItem(this.mapToCartItem(this.products[index]));
+      } else {
+        this.products[index].customQuantity = false;
+        this.removeProduct(this.products[index]);
+      }
+    }
+  }
 
   calculateTotalPrice(quantity: number, price: number): string {
     return (quantity * price).toFixed(2);
   }
 
-  removeProduct(product: any) {
+  removeProduct(product: ExtendedProduct) {
     this.products = this.products.filter(p => !(p.id === product.id && p.color === product.color && p.size === product.size));
-    // this.products = [...this.products];
-    this.cartService.removeCartItem(product);
-  }
-
-  removeFromCart(item: CartItem) {
-    this.cartService.removeCartItem(item);
-    this.cartItems = this.cartService.getAllCartItems();
+    this.cartService.removeCartItem(this.mapToCartItem(product));
   }
 
   clearCart(): void {
@@ -99,8 +96,8 @@ export class CartComponent implements OnInit {
     this.products = [];
   }
 
-  trackByItems(index: number, item: CartItem): any {
-    return item.id + item.color;
+  trackByItems(index: number, item: ExtendedProduct): any {
+    return item.id + item.color + item.size;
   }
 
   getTotalPrice(): number {
@@ -117,5 +114,13 @@ export class CartComponent implements OnInit {
     window.location.href = `/product/${id}`;
   }
 
-
+  private mapToCartItem(product: ExtendedProduct): CartItem {
+    return {
+      id: product.id,
+      color: product.color,
+      size: product.size,
+      quantity: product.quantity,
+      colorName: product.colorName
+    };
+  }
 }
